51128c2fffefc793dcaa1bffc17c75c1944b245f
nazwaam <nazwaamanda251@gmail.com>
2024-12-22 18:16:05 +0700

    Add files via upload

---
+++ Modul-6/Soal 1/PRAK601-2410817320015-NazwaAmanda.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    int matriks[a][b];
+    for(int i = 0; i < a; ++i){
+        for(int j = 0; j < b; ++j){
+            scanf("%d", &matriks[i][j]);
+            printf("%d ", matriks[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
